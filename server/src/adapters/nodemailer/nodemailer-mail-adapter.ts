75d21977bec3c35b99bd388ef2469cd61a84d46b
shenzuni <xkillthex@gmail.com>
2022-05-08 01:34:55 -0300

    add server

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "3f28f624cf1df0",
+      pass: "694383b4199a57"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Gian <gianpmaule@gmail.com>',
+            subject,
+            html: body
+        });
+    }
+}
